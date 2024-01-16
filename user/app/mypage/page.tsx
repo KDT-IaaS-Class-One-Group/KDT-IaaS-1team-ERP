38b3acb54e2a75a03690cb4280154924856f30d7
JungYoungSick <junfas1216@gmail.com>
2024-01-16 13:55:47 +0900

    user/mypage 왼쪽 정보란 작업 완료(오른쪽 정보란 작업 진행)

--- user/app/mypage/page.tsx
+++ user/app/mypage/page.tsx
@@ -7,21 +7,22 @@ export default function MyPage() {
     <div className="flex w-lvw h-lvh p-8">
       {/* 왼쪽 영역 */}
       <div className="flex flex-col mr-8 w-1/4 h-3/5 border border-black p-4">
-        <div className="mb-4">
+        <div className="mb-8 text-center bg-green-500 w-full h-full">
+          {/* 첫 번째 div */}
+          <UserInfo />
+        </div>
+        <div className="mb-8 text-center">
+          {/* 두 번째 div */}
           {/* 연락처 표시 */}
           <p className="text-lg font-semibold">
             연락처:
             {/* {yourContactNumber} */}
           </p>
-        </div>
-        <div className="mb-4">
           {/* 이메일 표시 */}
           <p className="text-lg font-semibold">
             이메일:
             {/* {yourEmail} */}
           </p>
-        </div>
-        <div>
           {/* Resign 버튼 */}
           <ResignButton />
         </div>
@@ -30,7 +31,7 @@ export default function MyPage() {
       {/* 오른쪽 영역 */}
       <div className="flex-1 bg-yellow-300 text-brown-800 p-8">
         {/* 두 컴포넌트 추가 */}
-        <UserInfo />
+
         <OrderInfoButton />
       </div>
     </div>
