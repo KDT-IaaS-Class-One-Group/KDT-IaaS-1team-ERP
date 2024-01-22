7c867d883f5eda30277b213ff0cb79f0cd3e66fc
bangseunghee <bangseunghee1108@gmail.com>
2024-01-22 12:52:43 +0900

    상품등록페이지 규격 추가

--- admin/app/apply/page.tsx
+++ admin/app/apply/page.tsx
@@ -1,15 +1,27 @@
-"use client";
+'use client'
 
-import React from "react";
-import { useState } from "react";
+
+import { useState, useEffect, useRef } from 'react';
+
+type Category = {
+  cateName: string;
+};
+
 
 export default function Apply() {
-  const [category, setCategory] = useState("");
-  const [productName, setProductName] = useState("");
-  const [price, setPrice] = useState("");
-  const [stock, setStock] = useState("");
+  const [category, setCategory] = useState<Category[]>([]);
+  const [productName, setProductName] = useState('');
+  const [price, setPrice] = useState('');
+  const [stock, setStock] = useState('');
   const [image, setImage] = useState(null);
-  const [origin, setOrigin] = useState('');
+  const [standard, setStandard] = useState('');
+  const [isInitialCategory, setIsInitialCategory] = useState(true);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+
+  const standardList = ["특", "대", "중", "소"]
+
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
@@ -17,46 +29,69 @@ export default function Apply() {
     const formData = new FormData();
 
     if (image) {
-      formData.append("image", image);
+      formData.append('image', image);
     }
-    formData.append('cateName', category);
+
+    formData.append('cateName', selectedCategory);
     formData.append('productName', productName);
     formData.append('price', price);
     formData.append('stock', stock);
-    formData.append('origin', origin);
-
+    formData.append('standard', standard);
+    
     try {
-      const response = await fetch("/addProduct", {
-        method: "POST",
+      const response = await fetch('/addProduct', {
+        method: 'POST',
         body: formData,
       });
-
+  
       if (!response.ok) {
-        const errorMessage =
-          response.status === 400
-            ? "해당 상품명이 이미 존재합니다."
-            : "상품을 등록하는데 실패했습니다.";
+        const errorMessage = response.status === 400 ? '해당 상품명이 이미 존재합니다.' : '상품을 등록하는데 실패했습니다.';
         throw new Error(errorMessage);
       }
-
+  
       const data = await response.json();
       console.log(data.message);
-      alert("상품 등록 완료");
+      alert('상품 등록 완료');
     } catch (error) {
       console.error('Error adding product:', error);
       alert((error as Error).message);
     }
 
-    setCategory("");
-    setProductName("");
-    setPrice("");
-    setStock("");
+    if (fileInputRef.current) {
+      (fileInputRef.current as HTMLInputElement).value = '';
+    }
+  
+    setSelectedCategory('');
+    setProductName('');
+    setPrice('');
+    setStock('');
+    setStandard('');
     setImage(null);
   };
 
-  const handleImageChange = (event: any) => {
+
+  useEffect(() => {
+    // 카테고리 목록을 서버에서 불러와 설정
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch('/category');
+        if (!response.ok) {
+          throw new Error('카테고리를 불러오는데 실패했습니다.');
+        }
+        const data = await response.json();
+        setCategory(data); // 여기서 배열로 설정되어야 합니다.
+      } catch (error) {
+        console.error('Error fetching categories:', error);
+      }
+    };
+  
+    fetchCategories();
+  }, []);
+
+  const handleImageChange = (event : any) => {
     const selectedImage = event.target.files[0];
     setImage(selectedImage);
+    
   };
 
 
@@ -76,20 +111,31 @@ export default function Apply() {
                 accept="image/*"
                 onChange={handleImageChange}
                 className="mt-2 w-full max-w-md"
+                ref={fileInputRef}
               />
             </label>
           </div>
           <div className="mb-4">
-            <label className="text-2xl font-bold" style={{ lineHeight: "2" }}>
-              카테고리 :
-              <input
-                type="text"
-                value={category}
-                onChange={(e) => setCategory(e.target.value)}
-                className="border-b border-black outline-none pl-2 w-full border border-gray-300"
-              />
-            </label>
-          </div>
+          <label className="text-2xl font-bold" style={{ lineHeight: '2' }}>
+            카테고리 :
+            <select
+              value={selectedCategory}
+              onChange={(e) => setSelectedCategory(e.target.value)}
+              className="border-b outline-none pl-2 w-full border border-gray-300"
+            >
+              {isInitialCategory && (
+                <option value="" disabled hidden>
+                  카테고리를 선택해주세요.
+                </option>
+              )}
+              {category.map((cate) => (
+                <option key={cate.cateName} value={cate.cateName}>
+                  {cate.cateName}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
           <div className="mb-4">
             <label className="text-2xl font-bold" style={{ lineHeight: "2" }}>
               상품명 :
@@ -97,7 +143,7 @@ export default function Apply() {
                 type="text"
                 value={productName}
                 onChange={(e) => setProductName(e.target.value)}
-                className="border-b border-black outline-none pl-2 w-full border border-gray-300"
+                className="border-b outline-none pl-2 w-full border border-gray-300"
               />
             </label>
           </div>
@@ -108,7 +154,7 @@ export default function Apply() {
                 type="text"
                 value={price}
                 onChange={(e) => setPrice(e.target.value)}
-                className="border-b border-black outline-none pl-2 w-full border border-gray-300"
+                className="border-b outline-none pl-2 w-full border border-gray-300"
               />
             </label>
           </div>
@@ -119,8 +165,27 @@ export default function Apply() {
                 type="text"
                 value={stock}
                 onChange={(e) => setStock(e.target.value)}
-                className="border-b border-black outline-none pl-2 w-full border border-gray-300"
+                className="border-boutline-none pl-2 w-full border border-gray-300"
               />
+            </label>
+          </div>
+          <div className="mb-4">
+            <label className="text-2xl font-bold" style={{ lineHeight: "2" }}>
+              규격 :
+              <select
+                value={standard}
+                onChange={(e) => setStandard(e.target.value)}
+                className="border-b outline-none pl-2 w-full border border-gray-300"
+              >
+                <option value="" disabled hidden>
+                  규격을 선택해주세요.
+                </option>
+                {standardList.map((value) => (
+                  <option key={value} value={value}>
+                    {value}
+                  </option>
+                ))}
+              </select>
             </label>
           </div>
           <button
