f0966eb79dbfece7b30227eee3b34155368e764d
bangseunghee <bangseunghee1108@gmail.com>
2024-01-24 00:48:38 +0900

    로고 이미지 사이즈 수정

--- user/app/ui/layoutButtons/reloadButton.tsx
+++ user/app/ui/layoutButtons/reloadButton.tsx
@@ -1,16 +1,13 @@
-'use client'
- 
- 
+"use client";
+
 export default function ReloadButton() {
-  
-  const handleClick = () =>{
-    window.location.href='/'
-  }
+  const handleClick = () => {
+    window.location.href = "/";
+  };
 
   return (
     <button type="button" onClick={handleClick}>
-      <img src="/dyabya.png" alt="logo" />
-      {/* DyaBya */}
+      <img style={{ width: "8vw" }} src="/logo.png" alt="logo" />
     </button>
-  )
+  );
 }
